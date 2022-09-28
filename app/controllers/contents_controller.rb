33a0e60062ef3940ec4c1a108443467d3c006bfd
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-29 06:47:46 +0900

    ユーザー登録の雛形作成

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,10 +1,18 @@
 class ContentsController < ApplicationController
   def index
+    @user = User.find(current_user.id)
   end
 
   def new
+    @content = Content.new
   end
 
   def create
+    @content = Content.new
+    if @content.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    :nikname
+    :email
+    :password
+    :self_introduction
+  end
 end
