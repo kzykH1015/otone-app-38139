2dcfd63efd1581f8fecd58fff51d127bf0a2e97d
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-10-02 20:34:52 +0900

    作品投稿機能の基礎を実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,11 +8,17 @@ class ContentsController < ApplicationController
   end
 
   def create
-    @content = Content.new
+    @content = Content.new(content_params)
     if @content.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :category_id, :story_line, :release_date).merge(user_id: current_user.id)
   end
 end
