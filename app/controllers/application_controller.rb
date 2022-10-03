5ce1a1e86554c04329e5de65bf1bf9b8c86053ae
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-10-03 21:27:48 +0900

    ユーザー情報更新機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :self_introduction])
   end
 
   def basic_auth
