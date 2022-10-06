42012a8d45918efbff782668d2edfb8592e84218
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-10-06 14:55:16 +0900

    フォローボタンの見た目調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
     @contents = Content.where(user_id: @user.id)
--- test/controllers/follow_relations_controller_test.rb
+++ test/controllers/follow_relations_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class FollowRelationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get followings" do
+  test 'should get followings' do
     get follow_relations_followings_url
     assert_response :success
   end
 
-  test "should get followers" do
+  test 'should get followers' do
     get follow_relations_followers_url
     assert_response :success
   end
-
 end
