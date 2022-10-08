9853d071e3f243109e85160e77a7ad3fa4b5ed5a
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-10-09 00:12:17 +0900

    herokuでのActionCable本番環境対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://otone-app-38139.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['http://otone-app-38139.herokuapp.com', 'http://otone-app-38139.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
