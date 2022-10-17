7031fb576c3be52dea6c32fed69bfdd298f0aa0b
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-10-17 21:15:57 +0900

    content_formの単体テスト

---
+++ spec/factories/contents.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :content_form do
+    title             {"devilmaycry5"}
+    category_id       {1}
+    genre_name        {"devil"}
+    creator_name      {"capcom"}
+    story_line        {"arasuji"}
+    release_date      {2022-10-15}
+    user_id           {2}
+  end
+end
