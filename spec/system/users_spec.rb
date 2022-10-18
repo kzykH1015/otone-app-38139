e10eaf8217e7bf5d2c692a663957335dcd521fd2
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-10-18 17:27:20 +0900

    ユーザー結合テスト異常系の作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe "Users", type: :system do
       fill_in 'user_self_introduction', with: @user.self_introduction
       # Nextボタンを押すとネタバレ選択画面に遷移する
       find('input[name="commit"]').click
-
+      expect(page).to have_content("ネタバレ防止設定")
       # 各spoilerを選択する
       select '表示しない', from: 'spoiler[genre_spoiler_id]'
       select '表示しない', from: 'spoiler[creator_spoiler_id]'
@@ -50,15 +50,41 @@ RSpec.describe "Users", type: :system do
     end
   end
   context 'ユーザー新規登録ができないとき' do
-    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+    it '誤った情報ではユーザー新規登録ができずに登録ページへ戻る' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content("新規登録")
       # 新規登録ページへ移動する
-      # ユーザー情報を入力する
-      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
-      # 新規登録ページへ戻されることを確認する
+      visit new_user_registration_path
+      # ユーザー情報を入力しない
+      fill_in 'user_nickname', with: ''
+      fill_in 'user_email', with: ''
+      fill_in 'user_password', with: ''
+      fill_in 'user_password_confirmation', with: ''
+      fill_in 'user_self_introduction', with: ''
+      # Nextボタンを押しても画面が遷移しないことを確認する
+      expect(current_path).to eq(new_user_registration_path)
+      # 再度ユーザー情報を入力して画面を移動する
+      fill_in 'user_nickname', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
+      fill_in 'user_self_introduction', with: @user.self_introduction
+      # Nextボタンを押すとネタバレ選択画面に遷移する
+      find('input[name="commit"]').click
+      # 各spoilerを選択しない
+      select '---', from: 'spoiler[genre_spoiler_id]'
+      select '表示しない', from: 'spoiler[creator_spoiler_id]'
+      select '表示しない', from: 'spoiler[story_line_spoiler_id]'
+      select '表示しない', from: 'spoiler[release_date_spoiler_id]'
+      select '表示しない', from: 'spoiler[comment_spoiler_id]'
+      # Sign upボタンを押すとUserモデルのカウントが上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # ネタバレ選択画面に戻されることを確認する
+      expect(page).to have_content("ネタバレ防止設定")
     end
   end
-
-
 end
