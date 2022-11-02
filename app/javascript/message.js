c9730c89e32cb4654b528abb33192dd8dc45bf8c
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-11-02 19:48:31 +0900

    ページ移動前の確認メッセージを実装

--- app/javascript/message.js
+++ app/javascript/message.js
@@ -1,12 +1,15 @@
 function message () {
-  console.log('www')
   const moveEditBtn = document.getElementById("content-edit-btn");
   moveEditBtn.addEventListener('click', (e) => {
-    console.log('kkk')
-    e.preventDefault();
-    e.returnValue = "hokano"
+    const select_message = confirm("編集画面では作品の情報が全て見えてしまいます\n移動して編集しますか？");
+
+    if (select_message) {
+      console.log('idousimasita')
+    } else {
+      console.log('yameta')
+      e.preventDefault()
+    };
   });
 };
-
 
 window.addEventListener('load', message);
